3c63de9130a462ca9e30f2de3ee09fa732d222d8
HeathKnowles <ch.en.u4cys22055@ch.students.amrita.edu>
2024-05-25 16:03:26 +0530

    Check

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,6 +1,31 @@
+"use client";
+
 import { CardWrapper } from "./card-wrapper"
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+
+import { LoginSchema } from "@/schemas"
+
+import { Input } from "@/components/ui/input";
+
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+
 
 export const LoginForm = () => {
+    const form = useForm<z.infer<typeof LoginSchema>({
+        resolver: zodResolver(LoginSchema),
+        defaultValues: {
+            email: "",
+            password: "",
+        },
+    })
     return (
         <CardWrapper
           headerLabel="Welcome back"
@@ -8,7 +33,31 @@ export const LoginForm = () => {
           backButtonHref="/auth/register"
           showSocial
         >
-            Login Form
+            <Form {...form}>
+              <form 
+                onSubmit={form.handleSubmit(() => {})}
+                className="space-y-6"
+            >
+                <div className="space-y-4">
+                    <FormField 
+                      control={form.control}
+                      name="email"
+                      render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Email</FormLabel>
+                            <FormControl>
+                                <Input 
+                                  {...field}
+                                  placeholder="john.doe"
+                                />
+                            </FormControl>
+                        </FormItem>
+                      )}
+                />
+
+                    
+                </div>
+            </Form>
         </CardWrapper>
     )
 }
---
+++ components/background/bg1.tsx
@@ -0,0 +1,10 @@
+// components/CustomBackground.tsx
+const CustomBackground = () => {
+  return (
+    <div className="bg-custom-gradient bg-custom-size bg-custom-position h-screen">
+      {/* Your content here */}
+    </div>
+  );
+};
+
+export default CustomBackground;
